b0eb81829659c4cee86a333a5205b8dd64b339ef
Sniper <sniper_zheng@163.com>
2023-01-20 11:50:51 +0800

    prepare for interface

---
+++ LearningSwift/LearningSwift/Base/Const/ConstEncryptValues.swift
@@ -0,0 +1,13 @@
+//
+//  ConstEncryptValues.swift
+//  LearningSwift
+//
+//  Created by Sniper on 2023/1/20.
+//
+
+import Foundation
+
+struct ConstEncryptValues {
+    static let jokeAppId = "secret"
+    static let jokeAppSecret = "secret"
+}
